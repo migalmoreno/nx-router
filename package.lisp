9145798652b23634c5e644b31037352ef9c0d6ac
Miguel Ángel Moreno <mail@migalmoreno.com>
2022-09-08 10:54:26 +0200

    package.lisp: Import serapeum utilities

--- package.lisp
+++ package.lisp
@@ -9,4 +9,7 @@
                 #:current-buffer
                 #:url
                 #:buffer)
+  (:import-from #:serapeum
+                #:->
+                #:export-always)
   (:documentation "nx-router allows you to define composable and flexible routes for Nyxt."))
